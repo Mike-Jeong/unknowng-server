066fa344439a12ad191929ef5c29b151ef7de7c7
Mike-Jeong <alstlr65@gmail.com>
2023-04-09 22:17:41 +0900

    add : application-prod.yml 에 redis 설정 추가 fix : Optimize imports

--- src/main/java/com/example/unknowngserver/auth/service/AuthService.java
+++ src/main/java/com/example/unknowngserver/auth/service/AuthService.java
@@ -18,7 +18,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.session.data.redis.RedisIndexedSessionRepository;
-import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
